e273e73a8ef69776944dad42d0226370b335f239
MikyTuberr <pio1342@gmail.com>
2024-03-26 00:41:25 +0100

    config update

--- config.sh
+++ config.sh
@@ -2,5 +2,11 @@
 
 SRC_PATH="./src" 
 
-gcc $SRC_PATH/main.c $SRC_PATH/input_parse.c $SRC_PATH/builtins.c $SRC_PATH/core.c -o main -fsanitize=leak 
-./main
+gcc "$SRC_PATH/main.c" "$SRC_PATH/input_parse.c" "$SRC_PATH/builtins.c" "$SRC_PATH/core.c" -o main -fsanitize=leak
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful."
+    ./main
+else
+    echo "Error during compilation."
+fi
